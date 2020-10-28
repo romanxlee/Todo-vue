6e33591b8e2f53d44bb46ad74e96c970f99373f0
Roman Lee <romanxlee@yandex.ru>
2020-10-28 17:26:40 +0300

    First commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+
+        <p>Simple SPA with todo-list. Click link below</p>
+    
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
